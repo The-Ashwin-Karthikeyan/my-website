acb8262f96340cc2632e2deec8c1235198d7b57d
Ashwin Karthikeyan <ashwin@Ashwins-MacBook-Pro.local>
2025-02-16 23:09:12 -0500

    button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Whoa</h1>
       <p>I can like... say anything here.</p>
+      <MyButton/>
     </div>
   );
 }
